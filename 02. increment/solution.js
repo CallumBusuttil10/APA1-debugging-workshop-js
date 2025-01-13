6f09db7e821f03e80b26bb1339635fb62b392a8f
CallumBusuttil10 <166800341+CallumBusuttil10@users.noreply.github.com>
2025-01-13 11:56:43 +0000

    Test Suite Passed: 02.Increment

--- 02. increment/solution.js
+++ 02. increment/solution.js
@@ -1,5 +1,5 @@
 function increment(n) {
-  return n++;
+  return n+1;
 }
 
 module.exports = { increment };
@@ -8,3 +8,6 @@ module.exports = { increment };
 // Then call the function to check how it is actually working...
 
 // Write some comments down below with your findings... 
+
+
+//I expect this function to take a number and return the number + 1.
