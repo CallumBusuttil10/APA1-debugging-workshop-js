c264c8ed31187789cdcc772ba6b5ea5f4131828b
CallumBusuttil10 <166800341+CallumBusuttil10@users.noreply.github.com>
2025-01-13 12:59:16 +0000

    Test Suite Passed: 05.Pence-To-Pounds

--- 05. pence-to-pounds/solution.js
+++ 05. pence-to-pounds/solution.js
@@ -1,8 +1,13 @@
 function convertFromPenceToPounds(pence) {
   const pounds = pence / 100;
-  return `£${Math.round(pounds)}`; // Format to two decimal places
+  console.log(pounds)
+  return `£${pounds.toFixed(2)}`; // Format to two decimal places
 }
 
 module.exports = { convertFromPenceToPounds };
 
-convertFromPenceToPounds(1299); // should return "£12.99"
+console.log(convertFromPenceToPounds(1299)); // should return "£12.99"
+
+
+/*I expect this function to return the total ammount in pounds and include two decimal places
+regardless of if there are 0 pence or not */
